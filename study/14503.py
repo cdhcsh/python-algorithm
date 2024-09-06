0fce34ae8fd79cc3b0837701fca4b7e94a0c59fe
cdhcsh <cdhcsh@gmail.com>
2024-09-06 23:24:53 +0900

    9/6 로봇청소기

---
+++ study/14503.py
@@ -0,0 +1,29 @@
+import sys
+
+def solve(n:int,m:int,start:(int,int),sd:int,data:list[list:int])->int:
+    d = [[-1,0],[0,1],[1,0],[0,-1]]
+    answer = 0
+    x,y = start
+    while True:
+        if data[x][y] == 0:
+            data[x][y] = -1
+            answer += 1
+        out = False
+        for _ in range(4):
+            sd = (sd + 3) % 4
+            tx,ty = x+d[sd][0],y+d[sd][1]
+            if 0 <= tx < n and 0 <= ty < m and data[tx][ty] == 0:
+                x,y = tx,ty
+                break
+        else:
+            tx,ty = x-d[sd][0],y-d[sd][1]
+            if 0 <= tx < n and 0 <= ty < m and data[tx][ty] <= 0:
+                x,y = tx,ty
+            else: break
+    return answer
+
+
+n,m = map(int,sys.stdin.readline().split())
+x,y,d = map(int,sys.stdin.readline().split())
+data = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+print(solve(n,m,(x,y),d,data))
