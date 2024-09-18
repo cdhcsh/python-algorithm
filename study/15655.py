517d27fb0465749be999fc35574e14f87902ebda
cdhcsh <cdhcsh@gmail.com>
2024-09-19 02:51:34 +0900

    9/18 N과M (6,7)

---
+++ study/15655.py
@@ -0,0 +1,18 @@
+import sys
+
+def solve(m:int,data:list[int])->list[list[int]]:
+    answer = []
+    def _dfs(n:int,i:int,arr:list[int]):
+        if n == m:
+            answer.append(arr)
+            return
+        for k in range(i,len(data)):
+            _dfs(n+1,k+1,arr+[data[k]])
+    data.sort()
+    _dfs(0,0,[])
+    return answer
+
+n,m = map(int,sys.stdin.readline().split())
+data = list(map(int,sys.stdin.readline().split()))
+answer = solve(m,data)
+[print(*r) for r in answer]
